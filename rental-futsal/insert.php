f5f346d2abf00534ff3a5a69306a1e255a072fdd
MrF <111920725+Sijemboed@users.noreply.github.com>
2024-04-21 18:57:03 +0700

    Add files via upload

---
+++ rental-futsal/insert.php
@@ -0,0 +1,25 @@
+<?php
+// Koneksi
+include('connection.php');
+
+// Menyimpan semua nilai query string ke variabel
+$nama = $_POST['nama'];
+$no_telepon = $_POST['no_telepon'];
+$tanggal = $_POST['tanggal'];
+$waktu = $_POST['waktu'];
+$durasi = $_POST['durasi'];
+// $jumlah_pemain = $_POST['jumlah_pemain'];
+$no_lapangan = $_POST['no_lapangan'];
+// $harga = $_POST['harga'];
+// $deposito = $_POST['deposito'];
+$keterangan = $_POST['keterangan'];
+
+// Menambahkan ke database
+$insert = mysqli_query($connect, "INSERT INTO booking SET nama='$nama', no_telepon='$no_telepon', tanggal='$tanggal', waktu='$waktu', durasi='$durasi', no_lapangan='$no_lapangan', keterangan='$keterangan' ");
+
+// Event Handler
+if ($insert) {
+    echo "<script>alert('Data berhasil ditambahkan!'); window.location.replace('index.php');</script>";
+} else {
+    echo "Data gagal ditambahkan...";
+}
